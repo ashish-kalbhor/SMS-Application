55669cc2728c4d974231d9b80bfffc960959ed40
rutukulkarni <rutuja.kulkarni24@gmail.com>
2015-10-06 22:06:51 -0400

    First Commit : Developing SMS App

---
+++ SMS/app/src/main/java/com/sms/sms/SMS.java
@@ -0,0 +1,57 @@
+package com.sms.sms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.os.Vibrator;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.TextView;
+import android.content.Context;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SMS extends AppCompatActivity {
+
+    private Context context = null;
+    SmsManager smsmgr = null;
+    Vibrator vibe = null;
+    private EditText cNumber;
+    private EditText textMsgContent = null;
+    Button btnSend = null;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms);
+
+        btnSend = (Button)findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                cNumber = (EditText) findViewById(R.id.etPhoneNo);
+                textMsgContent = (EditText)findViewById((R.id.etSmsText));
+
+                context = context;
+                smsmgr = SmsManager.getDefault();
+                vibe = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+
+                Toast.makeText(context, "SMS Wishes Sent to " + cNumber + "..!!", Toast.LENGTH_SHORT).show();
+                smsmgr.sendTextMessage(cNumber.toString(), null, textMsgContent.toString(), null,
+                        null);
+                // SMS sent to contact number passed as Argument
+                vibe.vibrate(1000);
+
+
+            }
+        });
+
+                setContentView(R.layout.activity_sms);
+    }
+
+
+}
