ab8ea3bce784fa9c3b89a506be74e343afe1067a
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2015-10-07 00:40:09 -0400

    Updated context
    - context now is ApplicationContext
    - Button also moved a little

--- SMS/app/src/main/java/com/sms/sms/SMS.java
+++ SMS/app/src/main/java/com/sms/sms/SMS.java
@@ -27,30 +27,30 @@ public class SMS extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
+        context = getApplicationContext();
+
+        cNumber = (EditText) findViewById(R.id.etPhoneNo);
+        textMsgContent = (EditText)findViewById((R.id.etSmsText));
+
+        // context = context; Why this?
+        smsmgr = SmsManager.getDefault();
+        vibe = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 
         btnSend = (Button)findViewById(R.id.btnSend);
-        btnSend.setOnClickListener(new View.OnClickListener() {
+        btnSend.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-
-                cNumber = (EditText) findViewById(R.id.etPhoneNo);
-                textMsgContent = (EditText)findViewById((R.id.etSmsText));
-
-                context = context;
-                smsmgr = SmsManager.getDefault();
-                vibe = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-
+            public void onClick(View v)
+            {
                 Toast.makeText(context, "SMS Wishes Sent to " + cNumber + "..!!", Toast.LENGTH_SHORT).show();
                 smsmgr.sendTextMessage(cNumber.toString(), null, textMsgContent.toString(), null,
                         null);
                 // SMS sent to contact number passed as Argument
                 vibe.vibrate(1000);
-
-
             }
         });
 
-                setContentView(R.layout.activity_sms);
+        setContentView(R.layout.activity_sms);
     }
 
 
